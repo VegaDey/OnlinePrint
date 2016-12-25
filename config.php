261c8102ea93e28a2762040598a675a80a9a99df
Fidgeter <Fidgeter@mail.ru>
2016-12-25 03:35:43 -0800

    Update config.php

--- config.php
+++ config.php
@@ -1,26 +1,29 @@
 <?php
-$host = 'localhost'; 		// адрес сервера
-$database = 'bdprint';	 	// имя базы данных
-$user = 'root'; 		// имя пользователя
-$password = ''; 		// пароль
+$host = 'localhost';      // Host
+$database = 'bdprint';    // Name BD
+$user = 'root'; 		      // Name user
+$password = ''; 		      // Password user
 
-error_reporting(E_ALL); 	// Показать ошибки PHP - 1
-ini_set("display_errors", 1);	// Не показывать ошибки PHP - 0
+error_reporting(E_ALL);         // Visible PHP error - 1
+ini_set("display_errors", 1);   // Hide PHP error - 0
 
-//Прочие настройки
-$ver = 'OnlinePrint CMS v 1.0.0';	// Версия сборки
-$year = '2015';				// год сборки
+# Other setting
+$ver = 'OnlinePrint CMS v 1.0.0';   // Version build
+$year = '2015';                     // Year build
 
-//Данные на входа админ панель
-$adminuser = 'admin'; 		// имя пользователя
-$adminpass = 'admin'; 		// пароль
+# Admin login
+$adminuser = 'admin'; 		// Admin name
+$adminpass = 'admin'; 		// Admin password
 
-//Данные о вашей компании
-$nameshop = 'ИП Газиев Денис Фатыхович';	//Название вашей компании
-$phoneshop = '+7 (920) 933-14-50';	//Телефон вашей компании
-$emailshop = 'dsmurom@list.ru';		//E-mail вашей компании
+# Data your company
+$nameshop = 'Denis & CO';             // Name your company
+$phoneshop = '+7 (920) 933-14-50';    // Phone you company
+$emailshop = 'dsmurom@list.ru';       // E-mail you company
 
-//ОПОВЕЩЕНИЯ НА САЙТЕ
+$sitename = 'OnlinePrint CMS';				//Name your site
+$keywords = 'OnlinePrint, CMS, Print, Online';		//Enter keywords
+$description = 'Online ordering of printed products';	//Enter keywords
+
+# Allert site
 $allertshop = 'Ваш аказ успешно прошел, в ближайшее время с вами сяжется наш менеджер!<br><br>Так же вы можете позвонить по телефону ';	//Успешный заказ shop.php
-
 ?> 
